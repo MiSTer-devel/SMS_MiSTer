ca9e8ce11c818db54f5f83cc0c31faf1e1ad52a5
Gyorgy Szombathelyi <gyurco@freemail.hu>
2019-01-02 00:50:17 +0100

    VDP: Use the same counter progressing as in Genesis H32 mode

--- pal_video.vhd
+++ pal_video.vhd
@@ -27,34 +27,32 @@ begin
 	begin
 		if rising_edge(clk) then
 			if ce_pix = '1' then
-				if hcount=511 then
-					hcount <= (others => '0');
+				if hcount=256 then
+					vcount <= vcount + 1;
+					if vcount = 242 then
+						vcount <= vcount + 201;
+					elsif vcount = 476 then
+						vsync <= '0';
+					elsif vcount = 471 then
+						vsync <= '1';
+					end if;
+				end if;
+
+				hcount <= hcount + 1;
+				-- HCounter: 0-295, 466-511 = 342 steps
+				if hcount = 295 then
+					hcount <= conv_std_logic_vector(466,9);
+				end if;
+				if hcount = 288 then
+					hsync <= '1';
+				elsif hcount = 482 then
 					hsync <= '0';
-					if vcount=511 then
-						vcount <= (others=>'0');
-					else
-						vcount <= vcount + 1;
-						if vcount = 242 then
-							vcount <= vcount + 201;
-						elsif vcount = 476 then
-							vsync <= '0';
-						elsif vcount = 471 then
-							vsync <= '1';
-						end if;
-					end if;
-				else
-					hcount <= hcount + 1;
-					--if hcount = 488 then
-					if hcount = 317 then
-						hcount <= hcount + 171;
-						hsync <= '1';
-					end if;
 				end if;
 			end if;
 		end if;
 	end process;
-	
-	x	<= hcount-24;
+
+	x	<= hcount;
 	y	<= vcount;
 
 	process (clk)
@@ -62,7 +60,7 @@ begin
 		if rising_edge(clk) then
 			if ce_pix = '1' then
 --				if ((hcount>=72 and hcount<232) or (gg='0' and (hcount>=24 and hcount<280))) then
-				if (hcount>=24 and hcount<280) then
+				if (hcount>=0 and hcount<256) then
 					hblank <= '0';
 				else
 					hblank <= '1';
