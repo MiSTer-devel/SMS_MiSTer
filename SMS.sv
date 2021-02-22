a2a73ac454f26d695aff457cd837f9eeec6a1fac
sorgelig <pour.garbage@gmail.com>
2021-02-22 22:33:42 +0800

    Update sys. HV-Integer.

--- SMS.sv
+++ SMS.sv
@@ -39,8 +39,9 @@ module emu
 	output        CE_PIXEL,
 
 	//Video aspect ratio for HDMI. Most retro systems have ratio 4:3.
-	output [11:0] VIDEO_ARX,
-	output [11:0] VIDEO_ARY,
+	//if VIDEO_ARX[12] or VIDEO_ARY[12] is set then [11:0] contains scaled size instead of aspect ratio.
+	output [12:0] VIDEO_ARX,
+	output [12:0] VIDEO_ARY,
 
 	output  [7:0] VGA_R,
 	output  [7:0] VGA_G,
@@ -191,14 +192,15 @@ always @(posedge CLK_VIDEO) en216p <= ((HDMI_WIDTH == 1920) && (HDMI_HEIGHT == 1
 
 wire [1:0] ar = status[27:26];
 wire vga_de;
-video_crop video_crop
+video_freak video_freak
 (
 	.*,
 	.VGA_DE_IN(vga_de),
 	.ARX((!ar) ? (border ? 12'd47 : 12'd32) : (ar - 1'd1)),
 	.ARY((!ar) ? (border ? 12'd35 : 12'd21) : 12'd0),
 	.CROP_SIZE(en216p ? 10'd216 : 10'd0),
-	.CROP_OFF(0)
+	.CROP_OFF(0),
+	.SCALE(status[31:30])
 );
 
 
@@ -207,7 +209,7 @@ video_crop video_crop
 // 0         1         2         3          4         5         6   
 // 01234567890123456789012345678901 23456789012345678901234567890123
 // 0123456789ABCDEFGHIJKLMNOPQRSTUV 0123456789ABCDEFGHIJKLMNOPQRSTUV
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 `include "build_id.v"
 parameter CONF_STR = {
@@ -234,6 +236,8 @@ parameter CONF_STR = {
 	"P1O2,TV System,NTSC,PAL;",
 	"P1OQR,Aspect ratio,Original,Full Screen,[ARC1],[ARC2];",
 	"P1O35,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%,CRT 75%;",
+	"P1-;",
+	"P1OUV,Scale,Normal,V-Integer,Narrower HV-Integer,Wider HV-Integer;",
 	"P1-;",
 	"P1OD,Border,No,Yes;",
 	"D5P1OST,Masked left column,BG,Black,Cut;",
