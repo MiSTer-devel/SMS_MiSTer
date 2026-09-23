958d947f5411770a8cbc078facc3bbabb3265734
Fabricio Breve <fbreve@gmail.com>
2026-09-23 16:58:54 -0300

    Fix Evolution mapper bank savestate restore

--- rtl/mapper_ctrl.vhd
+++ rtl/mapper_ctrl.vhd
@@ -162,8 +162,7 @@ begin
 					bank3 <= x"03";
 				end if;
 				if mapper_set = '1' then
-					if mapper_evolution = '1' and
-					   evolution_ss_in(31 downto 16) = x"E132" then
+					if mapper_evolution = '1' then
 						bank0 <= mapper_in(47 downto 40);
 						bank1 <= mapper_in(39 downto 32);
 						bank2 <= mapper_in(31 downto 24);
